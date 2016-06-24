2d1d8666e5bfd8989e5a83d39d32cc4e59ad6f6b
Robert G <robert.guenzler@student.htw-berlin.de>
2016-06-24 15:07:03 +0200

    remove useless code

--- tests/bluetooth/src/sketch.ino
+++ tests/bluetooth/src/sketch.ino
@@ -8,58 +8,38 @@
 
 #include <SoftwareSerial.h>
 
-#define LEDpin 13
+#define LEDpin 8
 
-#define BluRX 0
-#define BluTX 1
+#define bluRX 2
+#define bluTX 3
 
-SoftwareSerial bluSerial(BluRX, BluTX);
+SoftwareSerial bluSerial(bluRX, bluTX);
+char val;
 
 void setup() {
     Serial.begin(9600);
 
-    pinMode(BluRX, INPUT);
-    pinMode(BluTX, OUTPUT);
-
-    setupBlueToothConnection();
-    
+    pinMode(13, OUTPUT);
     pinMode(LEDpin, OUTPUT);
+    
+    pinMode(bluRX, INPUT);
+    pinMode(bluTX, OUTPUT);
+
+    bluSerial.begin(9600);
+    Serial.println("starting...");
 }
 
 void loop() {
-    /* digitalWrite(13, LOW); */
-    digitalWrite(LEDpin, HIGH);
-    delay(2000);
+    digitalWrite(13, LOW);
     digitalWrite(LEDpin, LOW);
-    delay(2000);
-    
-    /* char val; */
-    /* while(1) { */
-    /*     if (Serial.available()) { */
-    /*         if (bluSerial.available()) { */
-    /*             val = bluSerial.read(); */
-    /*             Serial.print(val); */
-    /*         } */
-    /*     } */
-    /* } */
-    /* if (val == '1') { */
-    /*     digitalWrite(LEDpin, HIGH); */
-    /* } */
-    /* if (val == '0') { */
-    /*     digitalWrite(LEDpin, LOW); */
-    /* } */
-}
 
-void setupBlueToothConnection()
-{
- bluSerial.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
- bluSerial.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
- bluSerial.print("\r\n+STNA=SeeedBTSlave\r\n"); //set the bluetooth name as "SeeedBTSlave"
- bluSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
- bluSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
- delay(2000); // This delay is required.
- bluSerial.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable 
- Serial.println("The slave bluetooth is inquirable!");
- delay(2000); // This delay is required.
- bluSerial.flush();
+    if(bluSerial.available()) {
+      val = bluSerial.read();
+    }
+    
+    if (Serial.available()) {
+      Serial.println("! blutooth");
+      Serial.println(val);
+    }
+    delay(1000);
 }
