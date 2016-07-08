c77a123ae871902618798bef98c8ed15f8a663f9
Robert G <robert.guenzler@student.htw-berlin.de>
2016-07-08 13:02:04 +0200

    add last gas sensor

--- arduino/src/sketch.ino
+++ arduino/src/sketch.ino
@@ -2,7 +2,8 @@
 #include <TimerOne.h>
 
 #define MQ7PIN A0
-#define MQ2PIN A2
+#define MQ2PIN A1
+#define MQ135PIN A2
 #define DHTPIN 8
 #define DHTTYPE DHT11
 
@@ -14,6 +15,7 @@ float hid = 0;
 
 int mq7 = 0;
 int mq2 = 0;
+int mq135 = 0;
 
 char fmt [64];
 
@@ -37,13 +39,25 @@ int readMQ2() {
     }
 }
 
+int readMQ135() {
+    int value;
+    value = analogRead(MQ135PIN);
+    if (isnan(value)) {
+        return -1;
+    } else {
+        return value;
+    }
+
+}
+
 void serialize(char* fmt) {
-    sprintf(fmt, "mq7: %i, mq2: %i, hum: %i, tmp: %i, hid: %i;", mq7, mq2, (int)hum, (int)tmp, (int)(hid+0.5));
+    sprintf(fmt, "mq7:%i,mq2:%i,mq135:%i,hum:%i,tmp:%i,hid:%i;", mq7, mq2, mq135, (int)hum, (int)tmp, (int)(hid+0.5));
 }
 
 void callback() {
     mq7 = readMQ7();
     mq2 = readMQ2();
+    mq135 = readMQ135();
     hid = dht.computeHeatIndex(tmp, hum, false);
 
     serialize(fmt);
